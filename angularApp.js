ee9bae18b2d2d9e33a5f73466480ab94e80f6a3c
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-03-01 04:45:56 -0800

    added a profile page

--- angularApp.js
+++ angularApp.js
@@ -13,6 +13,10 @@ app.config([
     function($stateProvider, $urlRouterProvider){
         //For the /home requests, route it to a custom template, and use MainCtrl controller
         $stateProvider.state('search', {url:'/search', templateUrl:'/search.html', controller:'SearchCtrl'});
+
+        //Now route to the profile page
+        $stateProvider.state('profile', {url:'/profile/{id}', templateUrl:'/profile.html', controller:'ProfileCtrl'});
+
         //For everything else, route to home, for now
         $urlRouterProvider.otherwise('search');
     }
@@ -22,34 +26,18 @@ app.factory('employees', function(){
     var o = {
         employees: [
             {
-            name: 'shailesh',
-            title: 'manager',
-            phone: '911'
-            },
-            {
-                name: 'kk',
-                title: 'developer',
-                phone: '911'
-            },
-            {
+                id: 0,
                 name: 'shailesh',
                 title: 'manager',
-                phone: '911'
+                phone: '911',
+                skills: [{skill: 'C++', link: '', upvotes:0, comments:[]}]
             },
             {
+                id: 1,
                 name: 'kk',
                 title: 'developer',
-                phone: '911'
-            },
-            {
-                name: 'shailesh',
-                title: 'manager',
-                phone: '911'
-            },
-            {
-                name: 'kk',
-                title: 'developer',
-                phone: '911'
+                phone: '911',
+                skills: [{skill: 'C++', link: '', upvotes:0, comments:[]}]
             }
         ]
     };
@@ -67,5 +55,27 @@ app.controller('SearchCtrl', [
             $scope.employees = employees.employees;
         }
 
+}]);
+
+app.controller('ProfileCtrl', [
+    '$scope',
+    '$stateParams',
+    'employees', //here we give the controller the factory
+    function($scope, $stateParams, employees){
+        $scope.test = 'Hello world!';
+
+        $scope.employee = employees.employees[$stateParams.id];
+
+        $scope.addSkill = function(){
+            if($scope.skill === '') {return;}
+
+            $scope.employee.skills.push( { skill: $scope.skill, link: $scope.link, upvotes: 0, comments:[]});
+            $scope.skill = "";
+            $scope.link = "";
+        }
+
+        $scope.incrementUpvotes = function(post){
+            $scope.employee.skills.upvotes += 1;
+        }
     }]);
 
