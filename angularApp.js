609e99e2a02eab2b9fff051d802d5739358df513
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-03-01 02:20:14 -0800

    basic search box

--- angularApp.js
+++ angularApp.js
@@ -1,32 +1,32 @@
-<html>
-<head>
-    <title>Symantec skill search</title>
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.10/angular.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.10/angular-ui-router.js"></script>
-    <script src="angularApp.js"></script>
-</head>
+/**
+ * Created by kprasad on 3/1/16.
+ */
 
 
-<body ng-app="skillSearch" ng-controller="SearchCtrl">index
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <div ui-view></div>
-    </div>
-</div>
+//include any external angular modules in the array
+var app = angular.module('skill-search', ['ui.router']);
 
-<!--routing for the search page-->
-<script type="text/ng-template" id="/search.html">
-<div class="page-header">
-    <h1>Symantec skill locator</h1>
-</div>
-<form ng-submit="searchSkill()" style="margin-top:30px;">
-    <h3>Search for a skill</h3>
-    <div class="form-group">
-        <input type="text" class="form-control" placeholder="Java" ng-model="title"></input>
-    </div>
-    <button type="submit" class="btn btn-primary">Search</button>
-</form>
-</script>
-</body>
-</html>
+//I need to route different URLs, and configure my app with services needed
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        //For the /home requests, route it to a custom template, and use MainCtrl controller
+        $stateProvider.state('search', {url:'/search', templateUrl:'/search.html', controller:'SearchCtrl'});
+        //For everything else, route to home, for now
+        $urlRouterProvider.otherwise('search');
+    }
+]);
+
+//Now define our controller
+app.controller('SearchCtrl', [
+    '$scope',
+    function($scope, posts){
+
+        $scope.searchForSkills = function(){
+            if($scope.title === '') {return;}
+
+        }
+
+    }]);
+
